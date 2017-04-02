28498ec3ba6c580b10e8e949c9580544bac6052d
Michael Ekstrand <michaelekstrand@boisestate.edu>
2017-04-02 16:02:45 -0600

    Make item-item test work (fix memory limit)

--- lenskit-integration-tests/src/it/gradle/item-item-identical/verify.R
+++ lenskit-integration-tests/src/it/gradle/item-item-identical/verify.R
@@ -8,12 +8,17 @@ preds.wide = reshape(predictions[c("User", "Item", "Algorithm", "Prediction")],
                      direction="wide")
 
 message("Checking predictions")
+mismatches = is.na(preds.wide$Prediction.Standard) == is.na(preds.wide$Prediction.Normalizing)
+mismatches = mismatches == is.na(preds.wide$Standard.Userwise)
 pred.range = pmin(abs(preds.wide$Prediction.Standard - preds.wide$Prediction.Normalizing),
                   abs(preds.wide$Prediction.Standard - preds.wide$Prediction.Userwise))
 bad.preds = pred.range > 0.001
 nbad = sum(bad.preds, na.rm=TRUE)
 
-if (nbad > 0) {
+if (sum(!mismatches) > 0) {
+    print(head(subset(preds.wide, mismatches)))
+    stop("item-item didn't always match on success")
+} else if (nbad > 0) {
     print(head(subset(preds.wide, bad.preds)))
     stop("item-item had ", nbad, " bad predictions")
 } else {
